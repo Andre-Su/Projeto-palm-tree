330c63974e7bf97c27fe69defa52df4c8fd02037
Andre-Su <aascnpc@gmail.com>
2023-11-06 21:49:19 -0300

    edittest

--- assets/scripts/main-script.js
+++ assets/scripts/main-script.js
@@ -20,8 +20,7 @@ var contentBar = [
     `O carbono é um elemento químico de símbolo C, número atômico 6 e com massa atómica 12 u, sólido à temperatura ambiente.Como um membro do grupo 14 da tabela periódica, ele é um não metal e tetravalente — fazendo quatro elétrons disponíveis na forma de ligações covalentes. Há três isótopos naturais, com o 12C e 13C sendo estável, onde o 14C é radioativo, decompondo com uma meia-vida de aproximadamente 5730 anos. Ele é um dos poucos elementos químicos que já foram descobertos na antiguidade.`,
     "assets/img/placeholder.png"],
     ["Lítio",
-    `O lítio é um elemento químico de símbolo Li, número atômico 3 e massa atômica 7, contendo, na sua estrutura, três prótons e três elétrons. Na tabela periódica dos elementos químicos, pertencente ao grupo 1, dos elementos alcalinos. Sob condições normais de temperatura e pressão, é o metal mais leve e menos denso entre os elementos sólidos. Como todos os elementos alcalinos, o lítio tem reatividade e inflamabilidade elevada e, por essa razão, é, geralmente, estocado em óleo mineral. Quando é usinado, apresenta brilho, porém, em contato com o ar atmosférico ou na água, a superfície é corroída e adquire a cor cinza-prateada e manchas pretas.`,
-    "assets/img/placeholder.png"]
+    `O lítio é um elemento químico de símbolo Li, número atômico 3 e massa atômica 7, contendo, na sua estrutura, três prótons e três elétrons. Na tabela periódica dos elementos químicos, pertencente ao grupo 1, dos elementos alcalinos. Sob condições normais de temperatura e pressão, é o metal mais leve e menos denso entre os elementos sólidos. Como todos os elementos alcalinos, o lítio tem reatividade e inflamabilidade elevada e, por essa razão, é, geralmente, estocado em óleo mineral. Quando é usinado, apresenta brilho, porém, em contato com o ar atmosférico ou na água, a superfície é corroída e adquire a cor cinza-prateada e manchas pretas.`]
 ];
 var dtIndex = 0;
 
